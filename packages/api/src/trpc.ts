8cb462c019368ebc9e7b341d612e06a51b09dd0a
Santiago Rodriguez <05rmsantiago@gmail.com>
2024-10-06 17:13:23 -0300

    feat: install winston, and create log config

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -1,26 +1,28 @@
 /**
  * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
+ *
  * 1. You want to modify request context (see Part 1)
  * 2. You want to create a new middleware or type of procedure (see Part 3)
  *
  * tl;dr - this is where all the tRPC server stuff is created and plugged in.
- * The pieces you will need to use are documented accordingly near the end
+ * The pieces you will need to use are documented accordingly near the end.
  */
-import { TRPCError, initTRPC } from '@trpc/server';
+
+import { getServerSession, type Session } from '@acme/auth';
+import { prisma } from '@acme/db';
+import Logger from './logs';
+import { initTRPC, TRPCError } from '@trpc/server';
 import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
-import { getServerSession, type Session } from '@acme/auth';
-import { prisma } from '@acme/db';
 
 /**
- * 1. CONTEXT
+ * 1. CONTEXT.
  *
- * This section defines the "contexts" that are available in the backend API
+ * This section defines the "contexts" that are available in the backend API.
  *
  * These allow you to access things like the database, the session, etc, when
- * processing a request
- *
+ * processing a request.
  */
 type CreateContextOptions = {
   session: Session | null;
@@ -28,11 +30,13 @@ type CreateContextOptions = {
 
 /**
  * This helper generates the "internals" for a tRPC context. If you need to use
- * it, you can export it from here
+ * it, you can export it from here.
  *
  * Examples of things you may need it for:
- * - testing, so we dont have to mock Next.js' req/res
- * - trpc's `createSSGHelpers` where we don't have req/res
+ *
+ * - testing, so we dont have to mock Next.js' req/res.
+ * - trpc's `createSSGHelpers` where we don't have req/res.
+ *
  * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
  */
 const createInnerTRPCContext = (opts: CreateContextOptions) => {
@@ -44,7 +48,8 @@ const createInnerTRPCContext = (opts: CreateContextOptions) => {
 
 /**
  * This is the actual context you'll use in your router. It will be used to
- * process every request that goes through your tRPC endpoint
+ * process every request that goes through your tRPC endpoint.
+ *
  * @link https://trpc.io/docs/context
  */
 export const createTRPCContext = async (opts: CreateNextContextOptions) => {
@@ -59,10 +64,10 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
 };
 
 /**
- * 2. INITIALIZATION
+ * 2. INITIALIZATION.
  *
  * This is where the trpc api is initialized, connecting the context and
- * transformer
+ * transformer.
  */
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
@@ -81,27 +86,52 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
  * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
  *
  * These are the pieces you use to build your tRPC API. You should import these
- * a lot in the /src/server/api/routers folder
+ * a lot in the /src/server/api/routers folder.
  */
 
 /**
- * This is how you create new routers and subrouters in your tRPC API
+ * This is how you create new routers and subrouters in your tRPC API.
+ *
  * @see https://trpc.io/docs/router
  */
 export const createTRPCRouter = t.router;
 
 /**
- * Public (unauthed) procedure
+ * Definition of middlewares to intercept requests and log custom messages with
+ * Winston: a middleware is created for general logs, and another for error
+ * logs.
+ */
+
+const generalLoggerMiddleware = t.middleware(async ({ path, type, next }) => {
+  Logger.info(`🚀 ${type} - ${path}`);
+  const start = Date.now();
+  const result = await next();
+  const duration = Date.now() - start;
+  Logger.info(`✅ ${type} - ${path} - ${duration}ms`);
+  return result;
+});
+
+const errorLoggerMiddleware = t.middleware(async ({ path, type, next }) => {
+  try {
+    return await next();
+  } catch (error) {
+    Logger.error(`❌ ${type} - ${path} - ${error}`);
+    throw error;
+  }
+});
+
+/**
+ * Public (unauthed) procedure.
  *
  * This is the base piece you use to build new queries and mutations on your
  * tRPC API. It does not guarantee that a user querying is authorized, but you
- * can still access user session data if they are logged in
+ * can still access user session data if they are logged in.
  */
-export const publicProcedure = t.procedure;
+export const publicProcedure = t.procedure.use(generalLoggerMiddleware).use(errorLoggerMiddleware);
 
 /**
  * Reusable middleware that enforces users are logged in before running the
- * procedure
+ * procedure.
  */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session?.user) {
@@ -116,12 +146,15 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
 });
 
 /**
- * Protected (authed) procedure
+ * Protected (authed) procedure.
  *
  * If you want a query or mutation to ONLY be accessible to logged in users, use
  * this. It verifies the session is valid and guarantees ctx.session.user is not
- * null
+ * null.
  *
  * @see https://trpc.io/docs/procedures
  */
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
+export const protectedProcedure = t.procedure
+  .use(generalLoggerMiddleware)
+  .use(errorLoggerMiddleware)
+  .use(enforceUserIsAuthed);
