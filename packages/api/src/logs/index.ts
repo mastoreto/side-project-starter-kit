f4841e2d6764d4089568b997ed3b37a632b32a8a
Santiago Rodriguez <05rmsantiago@gmail.com>
2024-10-19 14:39:00 -0300

    feat: :construction: relocate logs files and add folder in gitignore

--- packages/api/src/logs/index.ts
+++ packages/api/src/logs/index.ts
@@ -4,8 +4,8 @@ const Logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.File({ filename: 'log_files/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'log_files/combined.log' }),
   ],
 });
 
